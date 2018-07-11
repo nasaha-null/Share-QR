6b56f970c0477dd8068a8458a3a4365412983ae2
nasaha-null <ryusei283@outlook.com>
2018-07-12 07:23:00 +0900

    first commit

---
+++ Assets/Script/QRReader.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ZXing;
+using ZXing.QrCode;
+
+public class QRmanager
+{
+    public string read(WebCamTexture cameraTexture)
+    {
+        BarcodeReader reader = new BarcodeReader();
+        Color32[] color = cameraTexture.GetPixels32();
+        int width = cameraTexture.width;
+        int height = cameraTexture.height;
+        Result result = reader.Decode(color,width,height);
+        if(result != null){
+            return result.Text;
+        }
+        return "-1";//エラー時
+    }
+}
+
+public class QRReader : MonoBehaviour
+{
+    [SerializeField]
+    private int width = 1920;
+    [SerializeField]
+    private int height = 1080;
+
+    [SerializeField]
+    private RawImage displayUI = null;
+
+    private WebCamTexture webCamTexture = null;
+    private QRmanager qrManager;
+
+    public Text resultText;
+
+    private IEnumerator Start()
+    {
+        //カメラの接続と許可の確認
+        if (WebCamTexture.devices.Length == 0)
+        {
+            Debug.LogFormat("Camera is not found.");
+            yield break;
+        }
+
+        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+        if (!Application.HasUserAuthorization(UserAuthorization.WebCam))
+        {
+            Debug.LogFormat("Camera is not allowed.");
+            yield break;
+        }
+
+        //取得したデバイスからテクスチャの生成
+        WebCamDevice[] userCameraDevice = WebCamTexture.devices;
+        webCamTexture = new WebCamTexture(userCameraDevice[0].name, width, height);
+
+        //UIのRawImageにwebCamTextureを設定
+        displayUI.texture = webCamTexture;
+
+        //撮影開始
+        webCamTexture.Play();
+
+        //qrコード管理クラス
+        this.qrManager = new QRmanager();
+    }
+
+    void Update()
+    {
+        //カメラから読み取り
+        resultText.text = this.qrManager.read(webCamTexture);
+    }
+}
---
+++ Assets/Script/ShareSample.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShareSample : MonoBehaviour
+{
+    public void ShareScreenShot()
+    {
+        StartCoroutine(Share());
+    }
+
+    IEnumerator Share()
+    {
+        //スクリーンショットの保存先と名前(重複対策に実行時間を付与)を設定
+        string fileName = String.Format("image_{0:yyyyMMdd_Hmmss}.png", DateTime.Now);
+        string imagePath = Application.persistentDataPath + "/" + fileName;
+
+        // スクリーンショットの撮影と待機
+        ScreenCapture.CaptureScreenshot(fileName);
+        yield return new WaitForEndOfFrame();
+
+        // Shareするメッセージの設定と待機
+        string text = "hoge";
+        string URL = "foo";
+        yield return new WaitForSeconds(1);
+
+        //Shareする
+        SocialConnector.SocialConnector.Share(text, URL, imagePath);
+    }
+}
